187562882232c7896ddf5b0c2bc740a99efcc14f
Miranda Marques <137963450+OMirandinha@users.noreply.github.com>
2026-09-13 18:48:16 -0300

    Refactor princess search logic and improve gameplay

--- EncontreAPrincesa.c
+++ EncontreAPrincesa.c
@@ -2,38 +2,84 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define TAMANHO 10
 
-int main(){
+// Protótipo da função
+int procurarPrincesa(int princesa, int *tentativas);
+
+int main() {
     unsigned char salas[TAMANHO];
     int tentativas = 5;
 
-    //Sorteia a sala da princesa
-    int princesa = rand() % TAMANHO;
+    // Inicializa o sorteio
+    srand(time(NULL));
 
-    for(int i = 0; i < TAMANHO; i++){
+    // Sorteia uma sala de 1 até 10
+    int princesa = rand() % TAMANHO + 1;
+
+    // Inicializa as salas
+    for (int i = 0; i < TAMANHO; i++) {
         salas[i] = '#';
     }
-    while(tentativas > 0){
-        for(int i = 0; i < TAMANHO; i++)
+
+    // Mostra as salas e inicia o jogo
+    while (tentativas > 0) {
+
+        for (int i = 0; i < TAMANHO; i++) {
             printf("%c", salas[i]);
+        }
 
-        printf("\nOnde está a princesa?");
-
+        printf("\nOnde está a princesa? ");
+        
         procurarPrincesa(princesa, &tentativas);
 
-
+        printf("\n");
     }
 
+    if (tentativas == 0) {
+        printf("Suas tentativas acabaram!\n");
+        printf("A princesa estava na sala %d.\n", princesa);
+    }
+
+    return 0;
 }
 
-int procurarPrincesa(int *tentativas, int *princesa){
-    int res;
-    scanf("%d", &res);
-    if(res != princesa){
-        *tentativas--;
-        printf("%d", tentativas);    }
-    else{
-        printf("Parabéns! Encontrou a princesa!"); }
+
+// Função recursiva
+int procurarPrincesa(int princesa, int *tentativas) {
+
+    int resposta;
+
+    // Lê a tentativa do jogador
+    scanf("%d", &resposta);
+
+    // Jogador encontrou a princesa
+    if (resposta == princesa) {
+        printf("Parabéns! Você encontrou a princesa!\n");
+        return 1;
+    }
+
+    // Diminui uma tentativa
+    (*tentativas)--;
+
+    // Verifica se ainda existem tentativas
+    if (*tentativas == 0) {
+        return 0;
+    }
+
+    // Dá uma pista
+    if (resposta < princesa) {
+        printf("Procure uma sala MAIOR!\n");
+    } else {
+        printf("Procure uma sala MENOR!\n");
+    }
+
+    printf("Tentativas restantes: %d\n", *tentativas);
+    printf("Tente novamente: ");
+
+    // RECUSIVIDADE:
+    // a função chama ela mesma
+    return procurarPrincesa(princesa, tentativas);
 }
