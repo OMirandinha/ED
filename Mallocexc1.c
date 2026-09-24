78bbf15dd7c0d7383a67fafdde009108dafedf13
Miranda Marques <137963450+OMirandinha@users.noreply.github.com>
2026-09-23 21:19:32 -0300

    Implement dynamic array allocation and summation
    Allocate an array of integers using malloc, fill it, and display the sum of the values.

---
+++ Mallocexc1.c
@@ -0,0 +1,28 @@
+//Leia um número n e aloque com malloc um vetor de n inteiros.
+//Preencha o vetor, mostre a soma dos valores e libere a memória.
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    int resultado = 0;
+    printf("Digite um número inteiro");
+    scanf("%d", &n);
+
+    int *vet = malloc(n * sizeof(int));
+
+    for(int i = 0; i < n * sizeof(int); i++){
+        printf("\n Digite o elemento %d do vetor.", i+1);
+        scanf("%d", &vet[i]);
+    }
+
+    for(int i = 0; i < n * sizeof(int); i++){
+        resultado += vet[i];
+    }
+
+    printf("A somatória dos valores foi %d", resultado);
+
+    free(vet);
+
+    return 0;
+}
